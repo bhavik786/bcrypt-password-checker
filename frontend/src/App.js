e601e8fe4b8bb924ecc10b484508fe19f69cbc21
Bhavik Gevariya <bgevariya20@gmail.com>
2025-05-13 12:41:50 -0400

    Added _redirect
    Added _redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row } from "reactstrap";
+import { Container } from "reactstrap";
 import "./styles/index.css";
 import Header from "./Components/Header";
 import EncryptCard from "./Components/EncryptCard.jsx";
